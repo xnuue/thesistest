23c03203c58b8bb80896e8965ce01aa9fd2f7eb1
1xShizuku <94627144+xnuue@users.noreply.github.com>
2024-05-30 17:37:20 +0800

    firebase conn

---
+++ firebase.js
@@ -0,0 +1,21 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCo7nh58UwLmF5w6XPc4erpJgHE2fD1-pE",
+    authDomain: "thesissandbox.firebaseapp.com",
+    projectId: "thesissandbox",
+    storageBucket: "thesissandbox.appspot.com",
+    messagingSenderId: "219383246422",
+    appId: "1:219383246422:web:137c8b5cf599e6734dd5f7",
+    measurementId: "G-DPPCXVN3HD"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
