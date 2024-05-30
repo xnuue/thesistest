6d0d22c2f9ba57dba18bfaf341cb22ea066831c3
1xShizuku <94627144+xnuue@users.noreply.github.com>
2024-05-30 18:40:13 +0800

    j

--- firebase.js
+++ firebase.js
@@ -1,6 +1,5 @@
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,4 +17,3 @@
 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
