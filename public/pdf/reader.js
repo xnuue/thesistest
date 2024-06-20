cbc21c289bdb51817941e8b074692217005be627
1xShizuku <94627144+xnuue@users.noreply.github.com>
2024-06-21 00:24:21 +0800

    h

---
+++ public/pdf/reader.js
@@ -0,0 +1,6 @@
+const url = "https://firebasestorage.googleapis.com/v0/b/thesissandbox.appspot.com/o/pdf%2FKenneth.pdf?alt=media&token=dc823bb9-c4f4-4c3d-80df-733e9142901d";
+
+(asnyc function(){
+
+    pdfjsLib.GlobalWOrkkerOptions.workerSrc = "//mozilla.github.io/pdf.js/build/pdf.worker.js";
+})();
